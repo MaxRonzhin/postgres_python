466ce08c898bd55c1f5a703a3a9a40658a390bab
maxronzhin <mronzhin@bk.ru>
2022-02-28 05:48:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+with open('1gbfile',"wb") as file:
+    file.seek(1073741824)
+    file.write(b"\0")
+    print('файл 1gbfile успешно создан')
+myfl = os.path.abspath('1gbfile')
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="postgres",
+                                  host="127.0.0.1",
+                                  port="5432")
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
+    sql_create_database = 'create database post_pyt_db'
+    cursor.execute(sql_create_database)
+    print("БазаДанных post_pyt_db успешно создана")
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="postgres",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="post_pyt_db")
+    connection.autocommit = True
+
+    with connection.cursor() as cursor:
+        cursor.execute('''CREATE TABLE big_file (file oid); ''')
+        connection.commit()
+        print("Таблица big_file успешно создана")
+
+    with connection.cursor() as cursor:
+        content = f"INSERT INTO big_file (file) VALUES (lo_import('{myfl}'));"
+        cursor.execute(content)
+        
+        connection.commit()
+        print("Файл успешно добавлен в таблицу")
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+
+finally:
+    if connection:
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
+        
